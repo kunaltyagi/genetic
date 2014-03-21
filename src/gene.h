60a3a9d1bc6ed20a635d2d73481d046bff1d3e95
kunaltyagi <tyagi.kunal@live.com>
2014-03-22 03:59:42 +0530

    started on general genetic framework

---
+++ src/gene.h
@@ -0,0 +1,25 @@
+#ifndef _CHROMOSOME_H_
+#define _CHROMOSOME_H_
+
+#include <iostream>
+
+using namespace std;
+
+namespace Genetic
+{
+class Chromosome
+{
+public:
+    // Constructors
+    Chromosome();
+    //Destructor
+    ~Chromosome();
+
+private:
+
+protected:
+
+};
+
+}
+#endif
